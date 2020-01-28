3df83882ab0265d5841d0b2ad8954f4186213cd9
arusakova <arusakova917@gmail.com>
2020-01-28 19:05:43 +0200

    Finished decomposition

--- yellow/week2/starter.cpp
+++ yellow/week2/starter.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
-#include <map>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
@@ -21,19 +21,27 @@ struct Query {
   vector<string> stops;
 };
 
+  void clearStructure(Query& q) {
+      q.bus = "";
+      q.stop = "";
+      q.stops.clear();
+  }
+
 istream& operator >> (istream& is, Query& q) {
   string operation_code;
   is >> operation_code;
 
+  clearStructure(q);
+
   if (operation_code == "NEW_BUS") {
       q.type = QueryType::NewBus;
+      string stop;
 
       is >> q.bus;
 
       int stop_count;
       is >> stop_count;
-      q.stops.resize(stop_count);
-      for (string& stop : q.stops) {
+      for (int i = 0; i < stop_count; i++) {
         is >> stop;
         q.stops.push_back(stop);
       }
@@ -55,79 +63,41 @@ istream& operator >> (istream& is, Query& q) {
 }
 
 struct BusesForStopResponse {
-  size_t nr_buses;
-  vector<string> buses;
+  vector<string> response;
 };
 
 ostream& operator << (ostream& os, const BusesForStopResponse& r) {
-  if(r.nr_buses == 0 ){
-      os << "No stop";
-  } else {
-      for(auto item : r.buses)
-        os << item << " ";
-  }
+  for(auto item : r.response)
+    os << item;
   return os;
 }
 
-bool operator==(const BusesForStopResponse& lhs, const BusesForStopResponse& rhs){
-     return lhs.nr_buses == rhs.nr_buses && lhs.buses == rhs.buses;
-}
+
 
 struct StopsForBusResponse {
-  map<string, vector<string>> stops_for_buses;
+    vector<string> response;
 };
 
 ostream& operator << (ostream& os, const StopsForBusResponse& r) {
-  if(r.stops_for_buses.empty()) {
-      os << "No bus";
-  } else {
-      for(auto item : r.stops_for_buses) {
-          os << "Stop " << item.first <<": ";
-          if(item.second.empty()) {
-              os << "no intercharge";
-          } else {
-              for(auto other_item : item.second) {
-                  if (item.first != other_item) {
-                    os << other_item <<" ";
-                  }
-              }
-          }
-          os << "\n";
-      }
-  }
+  for(auto item : r.response)
+    os << item;
   return os;
-}
-bool operator==(const StopsForBusResponse& lhs, const StopsForBusResponse& rhs){
-     return lhs.stops_for_buses == rhs.stops_for_buses;
 }
 
 struct AllBusesResponse {
-  map<string, vector<string>> stops_to_buses;
+  vector<string> response;
 };
 
-bool operator==(const AllBusesResponse& lhs, const AllBusesResponse& rhs){
-     return lhs.stops_to_buses == rhs.stops_to_buses;
-}
-
 ostream& operator << (ostream& os, const AllBusesResponse& r) {
-  if (r.stops_to_buses.empty()) {
-      os << "No buses";
-  } else {
-      for(auto const &item : r.stops_to_buses) {
-          os <<"Bus " << item.first << ": ";
-          for (auto const &other_item : item.second) {
-              os << other_item << " ";
-          }
-          os << "\n";
-      }
-  }
+  for(auto item : r.response)
+    os << item;
   return os;
 }
 
 class BusManager {
 public:
   void AddBus(const string& bus, const vector<string>& stops) {
-    //Copy vecotr of stops for current bus
+    //Copy vector of stops for current bus
     buses_to_stops[bus] = stops;
 
     //Assign for each stop - current bus
@@ -138,112 +108,64 @@ public:
   }
 
   BusesForStopResponse GetBusesForStop(const string& stop) const {
-    if(stops_to_buses.find(stop) != stops_to_buses.end()) {
-        return {stops_to_buses.at(stop).size(), stops_to_buses.at(stop)};
-    } else {
-        return {0, {}};
-    }
+      BusesForStopResponse result;
+      if (stops_to_buses.count(stop) == 0) {
+          result.response.push_back("No stop");
+      } else {
+        for (const string& bus : stops_to_buses.at(stop)) {
+            result.response.push_back(bus);
+            result.response.push_back(" ");
+        }
+        result.response.push_back("\n");
+      }
+      return result;
   }
 
   StopsForBusResponse GetStopsForBus(const string& bus) const {
-    StopsForBusResponse result;
-    if(buses_to_stops.find(bus) != buses_to_stops.end()) {
-
-        for(auto item : buses_to_stops.at(bus)) {
-            if(stops_to_buses.at(item).size() == 1) {
-                result.stops_for_buses.insert({item, {}});
-            } else {
-                for(auto other_item : stops_to_buses.at(item)) {
-                    result.stops_for_buses[item].push_back(other_item);
-                }
+      StopsForBusResponse result;
+      if (buses_to_stops.count(bus) == 0) {
+          result.response.push_back("No bus");
+      } else {
+        for (const string& stop : buses_to_stops.at(bus)) {
+            result.response.push_back("Stop ");
+            result.response.push_back(stop);
+            result.response.push_back(": ");
+          if (stops_to_buses.at(stop).size() == 1) {
+              result.response.push_back("no interchange");
+          } else {
+            for (const string& other_bus : stops_to_buses.at(stop)) {
+              if (bus != other_bus) {
+                  result.response.push_back(other_bus);
+                  result.response.push_back(" ");
+              }
             }
+          }
+          result.response.push_back("\n");
         }
-    }
-    return result;
+      }
+      return result;
   }
 
   AllBusesResponse GetAllBuses() const {
-    return {buses_to_stops};
-  }
-
-  void RunAllTests()
-  {
-    testAddBus();
-    testGetBusesForStop();
+      AllBusesResponse result;
+      if (buses_to_stops.empty()) {
+          result.response.push_back("No buses");
+      } else {
+        for (const auto& bus_item : buses_to_stops) {
+            result.response.push_back("Bus ");
+            result.response.push_back(bus_item.first);
+            result.response.push_back(": ");
+          for (const string& stop : bus_item.second) {
+              result.response.push_back(stop);
+              result.response.push_back(" ");
+          }
+          result.response.push_back("\n");
+        }
+      }
+      return result;
   }
 
 private:
-
-  /********************** TESTS ******************************/
-  void testAddBus()
-  {
-      AddBus("32", {"A", "B", "C"});
-      map<string, vector<string>> b_to_s_expected = {{"32", {"A", "B", "C"}}};
-      assert(b_to_s_expected == buses_to_stops);
-
-      map<string, vector<string>> s_to_b_expected = {{"A", {"32"}}, {"B", {"32"}}, {"C", {"32"}}};
-      assert(s_to_b_expected == stops_to_buses);
-
-      AddBus("32K", {"B", "C", "D"});
-      b_to_s_expected = {{"32", {"A", "B", "C"}}, {"32K", {"B", "C", "D"}}};
-      assert(b_to_s_expected == buses_to_stops);
-
-      s_to_b_expected = {{"A", {"32"}}, {"B", {"32", "32K"}}, {"C", {"32", "32K"}}, {"D", {"32K"}}};
-      assert(s_to_b_expected == stops_to_buses);
-
-      buses_to_stops.clear();
-      stops_to_buses.clear();
-  }
-
-  void testGetBusesForStop()
-  {
-      AddBus("32", {"A", "B", "C"});
-      AddBus("32K", {"B", "C", "D"});
-      BusesForStopResponse actual = GetBusesForStop("E");
-      BusesForStopResponse expected = {0, {}};
-      assert(actual == expected);
-
-      actual = GetBusesForStop("D");
-      expected = {1, {"32K"}};
-      assert(actual == expected);
-
-      actual = GetBusesForStop("B");
-      expected = {2, {"32", "32K"}};
-      assert(actual == expected);
-
-      buses_to_stops.clear();
-      stops_to_buses.clear();
-  }
-
-void testGetStopsForBus()
-{
-      AddBus("32", {"A", "B", "C"});
-      AddBus("32K", {"B", "C", "D"});
-      AddBus("27", {"B", "C", "D"});
-
-      StopsForBusResponse actual = GetStopsForBus("32");
-      StopsForBusResponse expected = {{{"A", {}}, {"B", {"32K", "27"}}, {"C", {"32K", "27"}}}};
-      assert(actual == expected);
-
-      buses_to_stops.clear();
-      stops_to_buses.clear();
-}
-
-void testGetAllBuses()
-{
-      AddBus("32", {"A", "B", "C"});
-      AddBus("32K", {"B", "C", "D"});
-      AddBus("950", {"B", "C", "D"});
-      AddBus("272", {"B", "C", "D"});
-
-      AllBusesResponse actual = GetAllBuses();
-      AllBusesResponse expected = {{{"272", {"B", "C", "D"}}, {"272", {"B", "C", "D"}},
-                                    {"272", {"B", "C", "D"}}, {"272", {"B", "C", "D"}}}};
-      assert(actual == expected);
-
-      buses_to_stops.clear();
-      stops_to_buses.clear();
-}
 
   //Buses drives through given stop
   //Stop - buses
@@ -262,8 +184,8 @@ int main() {
  /* BusManager bm;
   bm.RunAllTests();
 
-  return 0; */
-
+  return 0;
+*/
   int query_count;
   Query q;
 
